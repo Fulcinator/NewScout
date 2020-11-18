6a16cb64b19cde27e43cf7ff170833d90c735992
Fulcinator <tommasofulcini@libero.it>
2020-11-18 12:27:33 +0100

    Aggiunto la pagina che printa delle statistiche, manca ancora qualche metodo

--- src/plugin/Page.java
+++ src/plugin/Page.java
@@ -1,5 +1,41 @@
 package plugin;
 
+import java.util.ArrayList;
+
 public class Page {
 
+	private ArrayList<String> interactions;
+	private int totalWidget;
+	private int activeWidget;
+	String id;
+	
+	public String getId() {
+		return id;
+	}
+
+	public Page(int totalWidget) {
+		this.totalWidget = totalWidget;
+		activeWidget = 0;
+		interactions = new ArrayList<>();
+	}
+	
+	public int getTotalWidget() {
+		return totalWidget;
+	}
+
+	public ArrayList<String> getInteractions() {
+		return interactions;
+	}
+
+	public int getActiveWidget() {
+		return activeWidget;
+	}
+	
+	public void printCurrentStats() {
+		System.out.println("Current page coverage is " + ((double) activeWidget/totalWidget *100 ) + "%");
+	}
+	
+	public void printStatsInteractions() {
+		System.out.println("There have been " + interactions.size() + " over the " + activeWidget + " active Widget");
+	}
 }
